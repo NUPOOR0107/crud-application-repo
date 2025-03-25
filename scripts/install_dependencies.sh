6c74d99adb88ed09c84df7d601fab8eb44aae770
NUPOOR0107 <166497756+NUPOOR0107@users.noreply.github.com>
2025-03-25 13:03:31 +0530

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -5,6 +5,13 @@ echo "Installing dependencies..."
 mkdir -p /home/ec2-user/banking_crud
 cd /home/ec2-user/banking_crud
 
+python --version
+python3 --version
+
+# Install Pip
+curl -O https://bootstrap.pypa.io/get-pip.py
+# python3 get-pip.py
+
 # Install Python dependencies
 pip3 install --no-cache-dir -r requirements.txt
 
