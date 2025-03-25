512eaa7194d54fa7422ce01733d4acfbf0a61caa
NUPOOR0107 <166497756+NUPOOR0107@users.noreply.github.com>
2025-03-25 12:56:14 +0530

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -2,7 +2,7 @@
 set -e  # Exit immediately if a command exits with a non-zero status
 
 echo "Installing dependencies..."
-
+mkdir -p /home/ec2-user/banking_crud
 cd /home/ec2-user/banking_crud
 
 # Install Python dependencies
