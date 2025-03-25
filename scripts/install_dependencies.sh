d82155b438512efb65e6e5b464d18bc29613fe98
NUPOOR0107 <166497756+NUPOOR0107@users.noreply.github.com>
2025-03-25 18:50:41 +0530

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -2,14 +2,24 @@
 set -e  # Exit immediately if a command exits with a non-zero status
 
 echo "Installing dependencies..."
-# if  mkdir -p /home/ec2-user/banking_crud
+
+# Navigate to the deployment directory
 cd /home/ec2-user/banking_crud
 
+# Unzip the application package
+echo "Extracting banking_crud.zip..."
+unzip -o banking_crud.zip
+rm -f banking_crud.zip  # Remove the ZIP file after extraction
+
+# Check Python version
 python3 --version
 
-# Install Pip
-curl -O https://bootstrap.pypa.io/get-pip.py
-python3 get-pip.py
+# Install Pip if not already installed
+if ! command -v pip3 &> /dev/null; then
+    echo "Installing Pip..."
+    curl -O https://bootstrap.pypa.io/get-pip.py
+    python3 get-pip.py
+fi
 
 # Install Python dependencies
 pip3 install --no-cache-dir -r requirements.txt
